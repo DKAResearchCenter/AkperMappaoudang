d7835dc1d443837c3689477c2414e156053fd9e6
Yovangga Anandhika <dhikaprivate@gmail.com>
2021-03-20 18:05:23 +0800

    Add Layout

---
+++ ANDROID/app/src/main/java/dka/product/akper/splashScreen.kt
@@ -0,0 +1,26 @@
+package dka.product.akper
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import dka.product.akper.databinding.UiActivitySplashscreenBinding
+
+class splashScreen : AppCompatActivity() {
+
+    private lateinit var i: Intent
+    private lateinit var mbinding: UiActivitySplashscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mbinding = DataBindingUtil.setContentView(this, R.layout.ui_activity_splashscreen)
+
+        Handler().postDelayed({
+            i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 2000)
+    }
+
+}
