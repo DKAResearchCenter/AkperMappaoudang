94526e9968117c25596958751c06c8ff6c22f5d1
diwan29 <rz29797@gmail.com>
2021-03-22 05:36:20 +0800

    tes

---
+++ ANDROID/app/src/main/java/dka/project/akper/view/AkunFragment.kt
@@ -0,0 +1,23 @@
+package dka.project.akper.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dka.project.akper.databinding.UiActivityBerandaFragmentAkunBinding
+
+class AkunFragment : Fragment() {
+
+    private lateinit var mbinding: UiActivityBerandaFragmentAkunBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mbinding = UiActivityBerandaFragmentAkunBinding.inflate(inflater, container, false)
+
+        return mbinding.root
+    }
+}
