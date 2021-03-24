cd3221aff1c2e0df2dc20a1d9aff9a32981db3e2
diwan29 <rz29797@gmail.com>
2021-03-25 03:50:08 +0800

    tes

--- ANDROID/app/src/main/java/dka/project/akper/view/LayananFragment.kt
+++ ANDROID/app/src/main/java/dka/project/akper/view/LayananFragment.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import dka.project.akper.databinding.UiActivityBerandaFragmentLayananUmumBinding
+import androidx.recyclerview.widget.GridLayoutManager
+import dka.project.akper.R
+import dka.project.akper.architecture.RecyclerView.LayananRecyclerView
+import dka.project.akper.databinding.UiActivityBerandaFragmentLayananBlankBinding
 
 class LayananFragment : Fragment() {
 
-    private lateinit var mbinding: UiActivityBerandaFragmentLayananUmumBinding
+    private lateinit var mbinding: UiActivityBerandaFragmentLayananBlankBinding
+
+    private var mdata: MutableList<LayananRecyclerView.data> = mutableListOf()
+
+    private lateinit var adapter: LayananRecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        adapter = LayananRecyclerView(requireActivity())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,9 +31,34 @@ class LayananFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        mbinding = UiActivityBerandaFragmentLayananUmumBinding.inflate(inflater, container, false)
+        mbinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.ui_activity_beranda_fragment_layanan_blank,
+            container,
+            false
+        )
 
         return mbinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mbinding.mrecyclelayanan.let {
+            it.layoutManager = GridLayoutManager(requireActivity(), 2)
+            it.adapter = adapter
+
+            mdata.apply {
+                clear()
+                add(LayananRecyclerView.data(R.drawable.ic_student, "Mahasiswa"))
+                add(LayananRecyclerView.data(R.drawable.ic_schedule, "Jadwal"))
+                add(LayananRecyclerView.data(R.drawable.ic_security_camera, "CCTV"))
+                add(LayananRecyclerView.data(R.drawable.ic_pin, "Map"))
+                add(LayananRecyclerView.data(R.drawable.ic_call_center__1_, "KHS Online"))
+            }
+            adapter.dataBaru()
+        }
+    }
+
 
 }
