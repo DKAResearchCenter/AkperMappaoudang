ed3946a98809f218e6c5d943b5f7211316945220
diwan29 <rz29797@gmail.com>
2021-03-27 02:08:43 +0800

    beranda layout

---
+++ ANDROID/app/src/main/java/dka/project/akper/ChatKontakActivity.kt
@@ -0,0 +1,18 @@
+package dka.project.akper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import dka.project.akper.databinding.UiActivityBerandaFragmentChatKontakBinding
+
+class ChatKontakActivity : AppCompatActivity() {
+
+    private lateinit var mbinding: UiActivityBerandaFragmentChatKontakBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mbinding =
+            DataBindingUtil.setContentView(this, R.layout.ui_activity_beranda_fragment_chat_kontak)
+    }
+}
--- ANDROID/app/src/main/java/dka/project/akper/view/ChatFragment.kt
+++ ANDROID/app/src/main/java/dka/project/akper/view/ChatFragment.kt
@@ -1,15 +1,18 @@
 package dka.project.akper.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dka.project.akper.ChatKontakActivity
 import dka.project.akper.databinding.UiActivityBerandaFragmentChatBinding
 
 class ChatFragment : Fragment() {
 
     private lateinit var mbinding: UiActivityBerandaFragmentChatBinding
+    private lateinit var i: Intent
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +20,13 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         mbinding = UiActivityBerandaFragmentChatBinding.inflate(inflater, container, false)
+
+        mbinding.let {
+            it.mkontak.setOnClickListener() {
+                i = Intent(requireActivity(), ChatKontakActivity::class.java)
+                startActivity(i)
+            }
+        }
 
         return mbinding.root
     }
